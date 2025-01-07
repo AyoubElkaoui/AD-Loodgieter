4b12c56420e317a49594ac8eb73dfe6744027b4e
AyoubElkaoui <elkaoui.a@gmail.com>
2025-01-07 03:24:33 +0100

    fixed google maps not showing centred

--- app/page.js
+++ app/page.js
@@ -171,6 +171,7 @@ export default function Home() {
                       <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d131921.06150352178!2d4.933146124625249!3d52.09296495760228!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c66f4339d32d37%3A0xd6c8fc4c19af4ae9!2sUtrecht!5e0!3m2!1sen!2snl!4v1735878699192!5m2!1sen!2snl"
                         width="100%"
+                        id="iframe-page"
                         height="350"
                         style={{ border: 0}}
                         allowFullScreen=""
