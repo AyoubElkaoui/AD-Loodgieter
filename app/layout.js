b8d6a5face00db7e93746b27c58c0c9f5ea424f6
AyoubElkaoui <elkaoui.a@gmail.com>
2025-01-08 10:19:07 +0100

    added tagmanager google

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import Script from "next/script";
 
 export const metadata = {
   title: 'Ad Loodgietersbedrijf',
@@ -12,11 +13,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
       <html lang="nl">
-      <body className="bg-gray-50 text-gray-800">
-      <Navbar />
-      <main className="container max-w-full mx-auto max-sm:w-full max-sm:max-w-screen-sm">{children}</main>
-      <Footer />
-      </body>
+        <body className="bg-gray-50 text-gray-800">
+          <Navbar />
+          <main className="container max-w-full mx-auto max-sm:w-full max-sm:max-w-screen-sm">{children}</main>
+          <Footer />
+          {/* Google Analytics */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-XNZF318PRR"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-XNZF318PRR');
+              `}
+          </Script>
+        </body>
       </html>
   );
 }
