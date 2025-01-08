583b75d18b3b2117258148486b584f4623a016e4
AyoubElkaoui <elkaoui.a@gmail.com>
2025-01-08 13:59:25 +0100

    fix build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    formats: ['image/avif', 'image/webp'], // Gebruik next-gen formats
-  },
-  experimental: {
-    optimizeCss: true, // Optimaliseer CSS
+    formats: ['image/avif', 'image/webp'],
   },
 };
 
