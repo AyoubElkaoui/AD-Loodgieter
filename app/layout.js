3aac77016be141b16b4c37cd607903684d9efb24
AyoubElkaoui <elkaoui.a@gmail.com>
2025-01-08 10:53:42 +0100

    GT added

--- app/layout.js
+++ app/layout.js
@@ -12,25 +12,35 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-      <html lang="nl">
-        <body className="bg-gray-50 text-gray-800">
-          <Navbar />
-          <main className="container max-w-full mx-auto max-sm:w-full max-sm:max-w-screen-sm">{children}</main>
-          <Footer />
-          {/* Google Analytics */}
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-XNZF318PRR"
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-XNZF318PRR');
-              `}
-          </Script>
-        </body>
-      </html>
+    <html lang="nl">
+    <head>
+      {/* Google Tag Manager */}
+      <Script id="gtm-head" strategy="afterInteractive">
+        {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-57S9L8VS');
+          `}
+      </Script>
+      {/* End Google Tag Manager */}
+    </head>
+    <body className="bg-gray-50 text-gray-800">
+    {/* Google Tag Manager (noscript) */}
+    <noscript>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-57S9L8VS"
+        height="0"
+        width="0"
+        style={{ display: 'none', visibility: 'hidden' }}
+      ></iframe>
+    </noscript>
+    {/* End Google Tag Manager */}
+    <Navbar/>
+    <main className="container max-w-full mx-auto max-sm:w-full max-sm:max-w-screen-sm">{children}</main>
+    <Footer/>
+    </body>
+    </html>
   );
 }
